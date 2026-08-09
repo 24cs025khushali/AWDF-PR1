18881a0c297696c75b55822fff09f4516f84042d
khushali gohel <24cs025@charusat.edu.in>
2026-08-09 12:04:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,35 @@
+function App(){
+  return(
+<>
+<div>
+  <h1>Khushali Gohel</h1>
+  <p>Hi,i am Khushali Gohel third year Student of Computer Science and Engineerimg</p>
+
+</div>
+
+<h2>About me</h2>
+<p>I am passionate about web development and always eager to learn new technologies, and i am interested in fashin designing and also interested in cloud and networking</p>
+
+
+<h2>Skills</h2>
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JavaScript</li>
+  <li>React</li>
+  <li>Node.js</li>
+  <li>Networking</li>
+</ul>
+
+<h2> Projects</h2>
+<ul>
+  <li>Grocery DElivery System</li>
+  <li>Face Recognision Based Attendebce System</li>
+
+</ul>
+</>
+  )
+
+}
+
+export default App;
